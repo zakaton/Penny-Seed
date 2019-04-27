d9da26057c2cec43a627dcb991c4b72d68caf998
Zack <zack@ukaton.com>
2019-04-26 20:53:46 -0700

    Revert "solidity and web3 wip"
    This reverts commit 4d59be3652406c226a0ce55c6df7e2d2ad1fcf65.

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Campaign = artifacts.require("./Campaign.sol");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(Campaign, "100000000000000000000", 1*24*60*60, "6500000000000000");
-};
